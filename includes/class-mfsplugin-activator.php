a061e8068fed8cda4b850db703d810de1d928686
Jerex Salingujay <98632127+jerexscodev@users.noreply.github.com>
2022-12-12 22:49:37 +0800

    initial commit

---
+++ includes/class-mfsplugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/jamainrex
+ * @since      1.0.0
+ *
+ * @package    Mfsplugin
+ * @subpackage Mfsplugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mfsplugin
+ * @subpackage Mfsplugin/includes
+ * @author     Jerex Lennon <skyguyverph@gmail.com>
+ */
+class Mfsplugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/partials/mfsplugin-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://github.com/jamainrex
+ * @since      1.0.0
+ *
+ * @package    Mfsplugin
+ * @subpackage Mfsplugin/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
